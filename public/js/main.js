12038dd0a6c0734032b08465bd68224aa6a32ae0
Joshua Keesee <joshuakeesee1@gmail.com>
2024-08-02 16:39:34 -0400

    Update server url

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 let user = {},
   profiles = {},
   rooms = {};
-const SERVER = "https://3l7m6r-3000.csb.app/";
+const SERVER = "https://nx8nkj-3000.csb.app/";
 const appName = document.title.split(" - ")[0];
 const chat = io(SERVER + "chat", {
   autoConnect: false,
